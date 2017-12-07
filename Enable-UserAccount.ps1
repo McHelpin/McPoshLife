11372ffff8c7e2603bbbe8c34b07a5446fe38877
Mike McAlpin <michaelpmcalpin@hotmail.com>
2017-12-07 10:53:30 -0600

    Add files via upload

---
+++ Enable-UserAccount.ps1
@@ -0,0 +1,6 @@
+﻿Function Enable-UserAccount {
+ [CmdletBinding()]
+Param( [Parameter(Mandatory = $true,HelpMessage = "Enter 'First Last'")]$UserName 
+)
+    Set-ADObject -Identity:"CN=$UserName,OU=Edina,OU=Accounts,DC=tcbmn,DC=net" -Replace:@{"userAccountControl"="512"} -Server:"EDDC01.tcbmn.net"
+}
