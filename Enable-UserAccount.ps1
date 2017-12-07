070071f0936f1aee90f1218b2ca1610c55af1520
Mike McAlpin <michaelpmcalpin@hotmail.com>
2017-12-07 15:25:14 -0600

    Delete Enable-UserAccount.ps1

--- Enable-UserAccount.ps1
+++
@@ -1,8 +0,0 @@
-﻿Function Enable-UserAccount {
- [CmdletBinding()]
-Param( [Parameter(Mandatory = $true,HelpMessage = "Enter 'First Last'")]$UserName,
-       [Parameter(Mandatory = $true,HelpMessage = 'Enter OU')]$OU,
-       [Parameter(Mandatory = $false,HelpMessage = 'Enter server name')]$Server = 'DC01.posh.net'
-)
-    Set-ADObject -Identity:"CN=$UserName,OU=$OU,DC=posh,DC=net" -Replace:@{"userAccountControl"="512"} -Server:$Server
-}
