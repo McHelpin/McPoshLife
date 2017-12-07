c9c4a2436f8470682e2a3ef94dccb1ec5b5e661e
Mike McAlpin <michaelpmcalpin@hotmail.com>
2017-12-07 14:51:58 -0600

    Update Enable-UserAccount.ps1
    added params for OU and server

--- Enable-UserAccount.ps1
+++ Enable-UserAccount.ps1
@@ -1,6 +1,8 @@
 ﻿Function Enable-UserAccount {
  [CmdletBinding()]
-Param( [Parameter(Mandatory = $true,HelpMessage = "Enter 'First Last'")]$UserName 
+Param( [Parameter(Mandatory = $true,HelpMessage = "Enter 'First Last'")]$UserName,
+       [Parameter(Mandatory = $true,HelpMessage = 'Enter OU')]$OU,
+       [Parameter(Mandatory = $false,HelpMessage = 'Enter server name')]$Server = 'DC01.posh.net'
 )
-    Set-ADObject -Identity:"CN=$UserName,OU=Edina,OU=Accounts,DC=tcbmn,DC=net" -Replace:@{"userAccountControl"="512"} -Server:"EDDC01.tcbmn.net"
+    Set-ADObject -Identity:"CN=$UserName,OU=$OU,DC=posh,DC=net" -Replace:@{"userAccountControl"="512"} -Server:$Server
 }
