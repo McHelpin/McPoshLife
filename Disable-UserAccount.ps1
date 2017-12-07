9b629369440f419321080c4fba446e658ef356e6
Mike McAlpin <michaelpmcalpin@hotmail.com>
2017-12-07 15:25:03 -0600

    Delete Disable-UserAccount.ps1

--- Disable-UserAccount.ps1
+++
@@ -1,8 +0,0 @@
-Function Disable-UserAccount {
- [CmdletBinding()]
- Param( [Parameter(Mandatory = $true,HelpMessage = "Enter 'First Last'")]$UserName,
-        [Parameter(Mandatory = $false,HelpMessage = 'Enter OU')]$OU = 'Accounts',
-        [Parameter(Mandatory = $false,HelpMessage = 'Enter server name')]$Server = 'DC01.posh.net'
-)
-    Set-ADObject -Identity:"CN=$UserName,OU=$OU,DC=posh,DC=net" -Replace:@{"userAccountControl"="514"} -Server:$Server
-}
